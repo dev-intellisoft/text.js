5b569299f9fbf201c3b9700a4ca4305dc2714792
Wellington Cunha <wellington@programer.com.br>
2019-11-03 11:16:52 -0300

    module ready for use

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path');
+
+class Textjs
+{
+    constructor()
+    {
+        this.locale = `en_US`
+        this.directory = `locale`
+        this.file = `main`
+
+    }
+
+    setDirectory = ( dir_name ) => this.directory = dir_name
+
+    setFile = ( file_name ) => this.file = file_name
+
+    setLocale = ( locale ) => this.locale = locale
+
+    getDirectory = () => this.directory
+
+    getFile = () => this.file
+
+    getLocale = () => this.locale
+
+    load = ( locale = null ) =>
+    {
+        let dir = path.dirname(require.main.filename);
+        dir = `${dir}/${this.getDirectory()}`
+
+        if ( !fs.existsSync(dir) )
+            return console.error(`Directory '${dir}' not found  on the root of your project!`)
+        dir = `${dir}/${locale || this.getLocale()}`
+
+        if ( !fs.existsSync(dir) )
+            return console.error(`Directory '${dir}' not found in your project!`)
+
+        dir = `${dir}/${this.getFile()}.json`
+        if ( !fs.existsSync(dir) )
+            return console.error(`Directory no found '${dir}' in your project!`)
+
+        return require(dir)
+
+    }
+
+    t = ( text, arr = {}, locale = null ) =>
+    {
+        const translator = this.load(locale)
+
+        text = translator[text] || text
+
+        for ( let key in arr )
+            text = text.replace(`{${key}}`, arr[key])
+
+        return text
+    }
+}
+
+module.exports = new Textjs()
